4be3f9404551d43d66f20c664ec0005e1cc48081
canarddu38 <71982379+canarddu38@users.noreply.github.com>
2021-10-01 18:03:00 +0200

    Update duckpvp.py

--- duckpvp.py
+++ duckpvp.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python
+import os
+import sys
 
+if not os.geteuid() == 0:
+    sys.exit("""\e[92m you must run this programm as root!""")
+
+print(""" \033[1;32m
+
+██████╗░██╗░░░██╗░█████╗░██╗░░██╗██████╗░██╗░░░██╗██████╗░
+██╔══██╗██║░░░██║██╔══██╗██║░██╔╝██╔══██╗██║░░░██║██╔══██╗
+██║░░██║██║░░░██║██║░░╚═╝█████═╝░██████╔╝╚██╗░██╔╝██████╔╝
+██║░░██║██║░░░██║██║░░██╗██╔═██╗░██╔═══╝░░╚████╔╝░██╔═══╝░
+██████╔╝╚██████╔╝╚█████╔╝██║░╚██╗██║░░░░░░░╚██╔╝░░██║░░░░░
+╚═════╝░░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░░░░░░░╚═╝░░░╚═╝░░░░░    \033[1;m""")
+
+def main():
+
+	print("\033[1;32m [\033[0;31m**\033[1;m] starting...\033[1;m")
+
+	print("""\033[1;32m
+TheDuckpvpProject by Canarddu38
+try help for more info...\033[1;m
+""")
+	system0 = raw_input(">>> ")
+	if system0 == "help":
+		print("...")
+		install = os.system("apt-get update && apt-get install -y nmap hping3 build-essential python-pip ruby-dev git libpcap-dev libgmp3-dev && pip install tabulate terminaltables")
+
+
+	else:
+		print("Please select the option 1 or 2")
+		main()
+main()
